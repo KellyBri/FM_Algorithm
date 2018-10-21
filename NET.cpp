fd2fb7fd1c6b28fdc35390837ea28daf5e1e593e
Kelly <kelly798186@gmail.com>
2018-10-22 01:59:50 +0800

    fix some errors

--- NET.cpp
+++ NET.cpp
@@ -21,14 +21,15 @@ void NET::setDistribution(bool s, int v){
 int NET::getID(){ return this->ID; }
 int NET::getSize(){ return this->cells.size(); }
 bool NET::getIsCut(){ return this->isCut; }
-std::set<int> NET::getCells(){ return this->cells; }
+std::vector<int> NET::getCells(){ return this->cells; }
+
 int NET::getDistribution(bool i){
     if(i) return this->distribution[1];
     else return this->distribution[0];
 }
 
 void NET::addCell(int cell){
-    this->cells.insert(cell);
+    this->cells.push_back(cell);
 }
 void NET::updateCut(){
     if( (this->distribution[0]>0) && (this->distribution[1]>0) ) this->isCut = true;
@@ -39,9 +40,9 @@ void NET::updateCut(){
 //print the informance of the net
 void NET::print(){
     std::cout << "ID: " << this->ID << "\tCut: " << getIsCut() << std::endl;
-    std::cout << "Disrtibution: (" << this->distribution[0] <<", " << this->distribution[1] << ")" << std::endl;
-    // std::cout <<"Cells: ";
-    // for(std::set<int>::iterator it = this->cells.begin(); it!=this->cells.end(); ++it)
-    //     std::cout << *it << "\t";
+    //std::cout << "Disrtibution: (" << this->distribution[0] <<", " << this->distribution[1] << ")" << std::endl;
+    std::cout <<"Cells: ";
+    for(std::vector<int>::iterator it = this->cells.begin(); it!=this->cells.end(); ++it)
+        std::cout << *it << "\t";
     std::cout<<std::endl;
 }
